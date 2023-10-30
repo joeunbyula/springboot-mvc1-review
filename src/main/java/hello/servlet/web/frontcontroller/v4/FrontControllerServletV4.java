9bf36450740d0ca46e455d6050d0df515b8f48cd
joeunbyula <gkdldy1109@gmail.com>
2023-10-30 09:51:56 +0900

    유연한 컨트롤러V5 - ControllerV4HandlerAdapter 추가

--- src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -3,7 +3,7 @@ package hello.servlet.web.frontcontroller.v4;
 import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
 import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
-import hello.servlet.web.frontcontroller.v4.controller.MemeberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,14 +13,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
 public class FrontControllerServletV4 extends HttpServlet {
 
     private Map<String, ControllerV4> controllerMap = new HashMap<>();
     public FrontControllerServletV4() {
-        controllerMap.put("/front-controller/v4/members/new-form", new MemeberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
         controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
         controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
     }
